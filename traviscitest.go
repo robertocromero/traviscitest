c3ecc86c2bc5e18196ffa24ce0bbd6940b85f31f
Roberto C. Romero <roberto.romero@safelayer.com>
2019-04-09 15:29:14 +0200

    A new commit to test

--- traviscitest.go
+++ traviscitest.go
@@ -7,7 +7,7 @@ func IsItFoo(word string) (bool, error) {
 	case "foo":
 		return true, nil
 	case "bar":
-		err := errors.New("Oh noooooo, it's bar!")
+		err := errors.New("ON NO, it's bar!")
 		return false, err
 	}
 	return false, nil

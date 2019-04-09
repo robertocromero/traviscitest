0a6c448c92774b5af7769761b4ef76abd5132c06
Roberto C. Romero <roberto.romero@safelayer.com>
2019-04-09 15:16:34 +0200

    This is only a test

---
+++ traviscitest.go
@@ -0,0 +1,15 @@
+package traviscitest
+
+import "errors"
+
+func IsItFoo(word string) (bool, error) {
+	switch word {
+	case "foo":
+		return true, nil
+	case "bar":
+		err := errors.New("Oh noooooo, it's bar!")
+		return false, err
+	}
+	return false, nil
+}
+

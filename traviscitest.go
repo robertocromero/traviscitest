b4ec78ed06bb5626d3d511b3c401c1d4ce7820c4
Roberto C. Romero <roberto.romero@safelayer.com>
2019-04-10 09:34:30 +0200

    test fixed

--- traviscitest.go
+++ traviscitest.go
@@ -1,18 +1,16 @@
 package traviscitest
 
 import "errors"
-import "fmt"
 
-func IsHelloWorld(word string) (bool, error) {
+func IsItFoo(word string) (bool, error) {
 	switch word {
-	case "hello":
-		fmt.Println("This is Hello Wooorld!")
+	case "foo":
 		return true, nil
-	case "notHello":
-		err := errors.New("What's this?")
-		fmt.Println("This is not Hello Wooooorld!")
+	case "bar":
+		err := errors.New("Oh noooooo, it's bar!")
 		return false, err
 	}
 	return false, nil
 }
+
 

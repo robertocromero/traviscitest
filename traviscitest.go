a15873c20180f3e158b3040ee292226fd097bea0
Roberto C. Romero <roberto.romero@safelayer.com>
2019-04-10 09:24:22 +0200

    test modified

--- traviscitest.go
+++ traviscitest.go
@@ -6,11 +6,11 @@ import "fmt"
 func IsHelloWorld(word string) (bool, error) {
 	switch word {
 	case "hello":
-		fmt.Println("This is Hello Woooorld!")
+		fmt.Println("This is Hello Wooorld!")
 		return true, nil
 	case "notHello":
 		err := errors.New("What's this?")
-		fmt.Println("This is not Hello World!")
+		fmt.Println("This is not Hello Wooooorld!")
 		return false, err
 	}
 	return false, nil
